45a6a4b34dfdc9b4bfa606582cccfe4d1592da56
vini <vinidemoraes05@gmail.com>
2026-02-25 00:20:49 -0300

    motion added - section CTA

--- gardez/src/app/components/landing/SectionCta.tsx
+++ gardez/src/app/components/landing/SectionCta.tsx
@@ -1,25 +1,35 @@
-import Image from "next/image"
-import styles from "@/app/components/styles/SectionCta.module.scss"
+"use client";
+
+import Image from "next/image";
+import styles from "@/app/components/styles/SectionCta.module.scss";
+import { motion } from "motion/react";
 
 export default function SectionCta() {
-    return (
-        <section className={styles.sectionCta}>
 
-            <div className={styles.textCta}>
-                <h2>Venda ou Alugue seu imóvel</h2>
-                <p>Rapido, Simples e Fácil</p>
-                <button className="btn-default">Comece já!</button>
-            </div>
+  return (
+    <section className={styles.sectionCta}>
+			
+      <motion.div  
+        className={styles.textCta}
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
+        >
+        <h2>Venda ou Alugue seu imóvel</h2>
+        <p>Rapido, Simples e Fácil</p>
+        <button className="btn-default">Comece já!</button>
+      </motion.div>
 
-            <div className={styles.imageCta}>
-                <Image 
-                    className="object-cover"
-                    src="/png/woman.png"
-                    alt="woman"
-                    fill
-                />
-            </div>
-
-        </section>
-    )
+      <motion.div 
+        className={styles.imageCta}
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        viewport={{ once: true }}
+        >
+        <Image className="object-cover" src="/png/woman.png" alt="woman" fill />
+      </motion.div>
+    </section>
+  );
 }
